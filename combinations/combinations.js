a792f3774ca4f9c86e83cbfdac9673a8003a2ef6
lorraine98 <limgytjd98@gmail.com>
2021-09-23 16:23:17 +0900

    Time: 148 ms (53.47%), Space: 44.5 MB (47.34%) - LeetHub

---
+++ combinations/combinations.js
@@ -0,0 +1,26 @@
+function combine(n, k) {
+    
+    const combinations = [];
+    const comb = [];
+    const nums = [...Array(n)].map((_, idx) => idx+1);
+    console.log(nums)
+    
+    function updateCombinations(pivot) {
+        if(k === comb.length) {
+           combinations.push([...comb]);
+           return;
+        }
+        
+        for(let i = pivot; i < nums.length; i++) {
+            if(comb.length > 0 && comb[comb.length-1] >= nums[i]) {
+                continue;
+            }
+            comb.push(nums[i]);
+            updateCombinations(pivot+1);
+            comb.pop();
+            
+        }
+    }
+    updateCombinations(0);
+    return combinations;
+};
