12b9602b3b9c18ea30926a83235946c3f0e05022
lorraine98 <limgytjd98@gmail.com>
2021-12-24 14:56:03 +0900

    Time: 84 ms (98.14%), Space: 45.7 MB (41.86%) - LeetHub

--- minesweeper/minesweeper.js
+++ minesweeper/minesweeper.js
@@ -4,42 +4,47 @@
  * @return {character[][]}
  */
 var updateBoard = function(board, click) {
+    const directX = [1, 1, 0, -1, -1, -1, 0, 1];
+    const directY = [0, -1, -1, -1, 0, 1, 1, 1];
+    const r = board.length, l = board[0].length;
+    let visited = Array.from(Array(r), () => Array(l).fill(false));
     
-    const R = board.length, C = board[0].length;
-    const clickR = click[0], clickC = click[1];
-
-    if (board[clickR][clickC]=='M') {
-        board[clickR][clickC] = 'X';
-        return board;
-    }
-
-    if (board[clickR][clickC]=='B') {
-        return board;
-    }
-
-    let tempR = clickR - 1, tempC = clickC - 1;
-    let mineCounter = 0;
-
-    for (let i=tempR;i<tempR+ 3;i++) {
-        for (let j=tempC;j<tempC + 3;j++) {
-            if (i < 0 || i >= R || j < 0 || j >= C) continue;
-            if (board[i][j]=='M') mineCounter++;
+    const checkRange = (x, y) => x>=0 && x<r && y>=0 && y<l;
+    
+    const dfs = (x, y) => {
+        visited[x][y] = true;
+        if(board[x][y] === 'M') {
+            board[x][y] = 'X';
+            return;
+        }
+        let isBlank = true;
+        let count = 0;
+        const next = [];
+        
+        for(let i=0; i<8; i++) {
+            const nx = x + directX[i];
+            const ny = y + directY[i];
+            
+            if(checkRange(nx, ny) && !visited[nx][ny]) {
+                if(board[nx][ny] === 'E') {
+                    next.push([nx, ny]);
+                } else if(board[nx][ny] === 'M') {
+                    count++;
+                    isBlank = false;
+                }
+            }
+        }
+        
+        if(isBlank) {
+            board[x][y] = 'B';
+            for(let i=0; i<next.length; i++) {
+                dfs(next[i][0], next[i][1]);
+            }
+        }
+        else {
+            board[x][y] = count.toString();
         }
     }
-
-    if (mineCounter != 0) {
-        board[clickR][clickC] = mineCounter.toString();
-        return board;
-    }
-
-    board[clickR][clickC] = 'B';
-
-    for (let i=tempR;i<tempR+ 3;i++) {
-        for (let j=tempC;j<tempC + 3;j++) {
-            if (i < 0 || i >= R || j < 0 || j >= C) continue;
-            board = updateBoard(board, [i, j]);
-        }
-    }
-    
+    dfs(click[0], click[1]);
     return board;
 };
