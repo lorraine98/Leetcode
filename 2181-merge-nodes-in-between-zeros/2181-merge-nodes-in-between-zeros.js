18ee2d6ee1ee1446d2001d7764dda12f10f7795c
Hyosung Lim <limgytjd98@gmail.com>
2022-04-15 01:46:48 +0900

    Time: 739 ms (33.23%), Space: 97 MB (87.94%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.js
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.js
@@ -11,12 +11,12 @@
  */
 
 var mergeNodes = function(head) {
-    let result = new ListNode(-1);
+    let result = new ListNode();
     let temp = result;
     
     while(head){
         if(head.val === 0 && head.next){
-            temp.next = new ListNode(0);
+            temp.next = new ListNode();
             temp = temp.next;
         }
         temp.val += head.val;
