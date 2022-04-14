ba0e22b666eaf21d2041a9343b661268d471dcca
Hyosung Lim <limgytjd98@gmail.com>
2022-04-15 01:43:19 +0900

    Time: 718 ms (36.47%), Space: 96.8 MB (89.80%) - LeetHub

---
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+var mergeNodes = function(head) {
+    let result = new ListNode();
+    let temp = result;
+    
+    while(head){
+        if(head.val === 0 && head.next){
+            temp.next = new ListNode();
+            temp = temp.next;
+        }
+        temp.val += head.val;
+        head = head.next;
+    }
+    return result.next;
+};
