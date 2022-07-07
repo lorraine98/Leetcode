43beb7a4fab09d49f03be36e51f88fd0a11adffb
Hyosung Lim <limgytjd98@gmail.com>
2022-07-08 01:48:44 +0900

    Time: 120 ms (47.35%), Space: 44.7 MB (54.81%) - LeetHub

---
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} boxTypes
+ * @param {number} truckSize
+ * @return {number}
+ */
+var maximumUnits = function(boxTypes, truckSize) {
+    let answer = 0;
+    
+    boxTypes.sort((a, b) => b[1] - a[1]);
+    
+    boxTypes.forEach(type => {
+        const [box, unit] = type;
+        if(truckSize >= box) {
+            answer += box * unit;
+            truckSize -= box;
+        }
+        else {
+            answer += truckSize * unit;
+            truckSize = 0;
+            return answer;
+        }
+    });
+    
+    return answer;
+};
