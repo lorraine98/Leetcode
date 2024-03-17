7619f08c9b73f0ddcf10555e7fbc69a9b1c5a8d0
Hyosung Lim <limgytjd98@gmail.com>
2024-03-17 19:06:06 +0900

    Time: 69 ms (33.62%), Space: 51.1 MB (77.15%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var missingNumber = function(nums) {
+    nums.sort((a, b) => a- b);
+
+    for(let i = 0; i <= nums.length; i++) {
+        if(i !== nums[i]) {
+            return i;
+        }
+    }
+};
