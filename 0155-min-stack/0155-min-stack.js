1e76dac3ba452dd1515397edf82478dd7c46f733
Hyosung Lim <limgytjd98@gmail.com>
2024-03-17 19:50:43 +0900

    Time: 110 ms (20.92%), Space: 58.9 MB (61.31%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.js
@@ -0,0 +1,49 @@
+
+var MinStack = function () {
+    this.elements = [];
+    this.minStack = [];
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+    this.elements.push(val);
+    if (this.minStack.length === 0 || val <= this.minStack[this.minStack.length - 1]) {
+        this.minStack.push(val);
+    }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+    const el = this.elements.pop();
+    if (el === this.minStack[this.minStack.length - 1]) {
+        this.minStack.pop();
+    }
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+    return this.elements[this.elements.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+    return this.minStack[this.minStack.length - 1];
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
