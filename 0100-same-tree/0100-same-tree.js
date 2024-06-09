7949c1a7be578ac93ea2ed273834ac4b570ed5da
Hyosung Lim <limgytjd98@gmail.com>
2024-06-09 19:32:01 +0900

    Time: 54 ms (48.85%), Space: 48.2 MB (98.99%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    let answer = true;
+
+    const dfs = (node1, node2) => {
+        if(!node1 && !node2) {
+            return;
+        }
+
+        if(node1?.val !== node2?.val) {
+            answer = false;
+            return;
+        }
+
+        dfs(node1.right, node2.right);
+        dfs(node1.left, node2.left);
+    }
+
+    dfs(p, q);
+    
+    return answer;
+};
