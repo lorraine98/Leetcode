0acc26ebaea78b71eecff2a2e909cfecee976bd8
Hyosung Lim <limgytjd98@gmail.com>
2024-06-23 18:01:51 +0900

    Time: 72 ms (70.1%), Space: 54.6 MB (97.26%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.js
+++ 0338-counting-bits/0338-counting-bits.js
@@ -2,16 +2,28 @@
  * @param {number} n
  * @return {number[]}
  */
+// var countBits = function (n) {
+//     const answer = Array(n + 1).fill(0);
+
+//     for (let i = 0; i <= n; i++) {
+//         const binary = i.toString(2);
+//         for (let j = 0; j < binary.length; j++) {
+//             if (binary[j] === '1') {
+//                 answer[i] = answer[i] + 1;
+//             }
+//         }
+//     }
+
+//     return answer;
+// };
+
 var countBits = function (n) {
     const answer = Array(n + 1).fill(0);
 
-    for (let i = 0; i <= n; i++) {
-        const binary = i.toString(2);
-        for (let j = 0; j < binary.length; j++) {
-            if (binary[j] === '1') {
-                answer[i] = answer[i] + 1;
-            }
-        }
+    for (let i = 1; i <= n; i++) {
+        // i & (i - 1)은 i의 가장 낮은 1 비트를 제거한 값입니다.
+        // 따라서, answer[i]는 answer[i & (i - 1)] + 1과 같습니다.
+        answer[i] = answer[i >> 1] + (i & 1);
     }
 
     return answer;
