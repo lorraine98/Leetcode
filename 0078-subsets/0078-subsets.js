92630b06cd9b2dd44966ccb68348e69d807cf5bc
Hyosung Lim <limgytjd98@gmail.com>
2024-07-28 02:13:59 +0900

    Time: 60 ms (68.04%), Space: 52.3 MB (65.23%) - LeetHub

--- 0078-subsets/0078-subsets.js
+++ 0078-subsets/0078-subsets.js
@@ -1,22 +1,20 @@
-const addSubset = (arr, num) => {
-    const len = arr.length;
-
-    for (let i = 0; i < len; i++) {
-        arr.push([...arr[i], num]);
-    }
-}
-
 var subsets = function (nums) {
-    if (nums.length === 1) {
-        return [[], nums]
+    if(nums.length === 1) {
+        return [[], nums];
     }
 
     const answer = [[]];
 
-    while (nums.length) {
+    while(nums.length) {
         const num = nums.pop();
-        addSubset(answer, num);
-    }
+        
+        const len = answer.length;
 
+        for(let i = 0; i < len; i++) {
+            answer.push([...answer[i], num]);
+        }
+    }
+    
     return answer;
 };
+
