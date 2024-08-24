bdd75b3feb36ea1885d42406481210da614b8ee7
Hyosung Lim <limgytjd98@gmail.com>
2024-08-25 02:23:33 +0900

    Time: 76 ms (86.93%), Space: 54.6 MB (72.99%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.js
@@ -4,66 +4,34 @@
  * @return {number[]}
  */
 
-// const modulus = 100000;
+const MOD = 100000;
 
-// for (let i = 0; i < 26; i++) {
-//     const char = String.fromCharCode(97 + i); // 'a'부터 'z'까지
-//     const value = char.charCodeAt(0) * (31 ** (i + 1));
-//     hashTable[char] = value % modulus; // 모듈로 연산으로 값을 줄임
-// }
-
-const hashTable = {
-    a: 97,
-    b: 196,
-    c: 297,
-    d: 400,
-    e: 505,
-    f: 612,
-    g: 721,
-    h: 832,
-    i: 945,
-    j: 1060,
-    k: 1177,
-    l: 1296,
-    m: 1417,
-    n: 1538,
-    o: 1661,
-    p: 1786,
-    q: 1913,
-    r: 2040,
-    s: 2171,
-    t: 2304,
-    u: 2439,
-    v: 2576,
-    w: 2715,
-    x: 2856,
-    y: 2999,
-    z: 3144
-}
-
-const getHashNumber = (str) => {
+const getHashNumber = (str, table) => {
     let res = 0;
     for (let i = 0; i < str.length; i++) {
-        res += hashTable[str[i]];
+        res += table[str[i]];
     }
     return res;
 }
 
 var findAnagrams = function (s, p) {
+    const hashTable = Array.from({ length: 26 }, (_, i) => {
+        const char = String.fromCharCode(97 + i);
+        const value = char.charCodeAt(0) * (31 ** (i + 1));
+        return { [char]: value % MOD };
+    }).reduce((acc, curr) => Object.assign(acc, curr), {});
+
     const answer = [];
+    const pHash = getHashNumber(p, hashTable);
 
-    const sLen = s.length;
-    const pLen = p.length;
-    const pHash = getHashNumber(p);
+    let cur = getHashNumber(s.slice(0, p.length), hashTable);
 
-    let cur = getHashNumber(s.slice(0, pLen));
-
-    for (let i = 0; i < sLen; i++) {
+    for (let i = 0; i < s.length; i++) {
         if (cur === pHash) {
             answer.push(i);
         }
 
-        cur = cur - hashTable[s[i]] + hashTable[s[i + pLen]];
+        cur = cur - hashTable[s[i]] + hashTable[s[i + p.length]];
     }
 
     return answer;
