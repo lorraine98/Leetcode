c63cdc7da8d7d83d4ab240627c1b6fedeb601807
Hyosung Lim <limgytjd98@gmail.com>
2025-03-09 22:17:14 +0900

    Time: 2 ms (57.17%), Space: 64.4 MB (22.02%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -4,8 +4,8 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    const len = nums.length;
-    const mod = k % len;
-    const arr = nums.splice(0, len - mod);
-    nums.push(...arr);
-};
+    k = k % nums.length;  // k가 nums.length보다 클 경우 처리
+    nums.reverse();
+    nums.splice(0, k, ...nums.slice(0, k).reverse());
+    nums.splice(k, nums.length - k, ...nums.slice(k).reverse());
+};;
